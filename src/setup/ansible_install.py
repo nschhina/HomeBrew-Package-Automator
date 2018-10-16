4026af9580b132494769094c01b21bf4dfdc83a8
Navraj Singh Chhina <navuchhina@live.com>
2018-10-16 11:50:30 -0400

    pip install only for mac

--- src/setup/ansible_install.py
+++ src/setup/ansible_install.py
@@ -1,12 +1,15 @@
 import subprocess
+import platform
 
 def pipinstaller():
-    pip_install_command = 'sudo easy_install pip'
-    subprocess.check_call(pip_install_command.split())
-    nose_install_command = 'sudo easy_install nose'
-    subprocess.check_call(nose_install_command.split())
-    tornado_install_command = 'sudo easy_install tornado'
-    subprocess.check_call(tornado_install_command.split())
+    if(platform.system()=='Darwin'):
+        pip_install_command = 'sudo easy_install pip'
+        subprocess.check_call(pip_install_command.split())
+        nose_install_command = 'sudo easy_install nose'
+        subprocess.check_call(nose_install_command.split())
+        tornado_install_command = 'sudo easy_install tornado'
+        subprocess.check_call(tornado_install_command.split())
+
 
 def ansibleinstaller():
     pip_install_ansible = 'pip install ansible'
